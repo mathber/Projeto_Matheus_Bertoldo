73833d79eb56855c60630c2b16644ceaf39f0c99
Matheus Bertoldo <matheus.bertoldo04@gmail.com>
2023-09-25 17:59:01 -0300

    Criado o Método MA3()

--- A.java
+++ A.java
@@ -25,5 +25,8 @@ public class A {
     public void MA2(){
         System.out.println("MA2");
     }
+    public void MA3(){
+        System.out.println("Alteração a classe A partir do clone");
+    }
   
 }
