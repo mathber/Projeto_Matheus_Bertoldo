76148fa57050779754ed1b823fafc3748df7ae92
Matheus Bertoldo <matheus.bertoldo04@gmail.com>
2023-09-25 17:52:18 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,15 @@
+/* Nome: Matheus Bertoldo   Matrícula: 202210360 */
+
+public class C {
+    
+    private String C1;
+    private int C2;
+
+    public void MC1(){
+        System.out.println("MC1");
+    }
+    public void MC2(){
+        System.out.println("MC2");
+    }
+
+}
